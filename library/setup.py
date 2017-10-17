16853dc4f2bd33ef2f7bcf90afd8c767cdf4724e
Phil Howard <phil@gadgetoid.com>
2017-10-17 15:09:19 +0100

    Added packaging, dropped copy of C code

--- library/setup.py
+++ library/setup.py
@@ -39,7 +39,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
 
 setup(
     name            = 'bme680',
-    version         = '0.0.1',
+    version         = '1.0.0',
     author          = 'Philip Howard',
     author_email    = 'phil@pimoroni.com',
     description     = """Python library for driving the Pimoroni BME680 Breakout""",
