714a57a98057c86dc4e6ee26266fc9a448bd5b1c
Phil Howard <phil@gadgetoid.com>
2018-08-28 21:25:35 +0100

    Intentional QA Fail

--- examples/indoor-air-quality.py
+++ examples/indoor-air-quality.py
@@ -19,7 +19,7 @@ sensor = bme680.BME680()
 # change the balance between accuracy and noise in
 # the data.
 
-sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_humidity_oversample(bme680.OS_2X)   
 sensor.set_pressure_oversample(bme680.OS_4X)
 sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
