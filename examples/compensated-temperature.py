069f12c60616cc67e23e336605f325ffa5fad171
Ian Grant <ian@iangrant.me>
2021-01-13 11:19:12 +0000

    Update examples to also catch RuntimeError on init

--- examples/compensated-temperature.py
+++ examples/compensated-temperature.py
@@ -15,7 +15,7 @@ Press Ctrl+C to exit!
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-except IOError:
+except (RuntimeError, IOError):
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
 # These oversampling settings can be tweaked to
